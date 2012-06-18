58da8bce968e152eef558669cb6821ae045d0434
Matthew Lewis <matt@mplewis.com>
2012-06-18 18:29:11 -0500

    Added major and minor scales; made code more modular; added more docs

--- musicScales.c
+++ musicScales.c
@@ -244,47 +244,85 @@ const float pianoFreq[88] = {nA0, nAs0, nB0, nC1, nCs1, nD1, nDs1, nE1, nF1, nFs
 #define maj7 11 // major seventh
 #define per8 12 // perfect octave
 
-/* freqScaleBlues
- * Takes in a starting keynum and a number of blues scale steps to move up/down (+/-)
- * Returns the freq value of that note as a float
- * The blues scale in Bb is as follows:
- *     Bb      Db      Eb      E      F      Ab      Bb        notes
- *      | min3  | maj2  | min2 | min2 | min3  | min2 |         interior intervals
- *      | min3  | per4  | triT | per5 | min7  | per8 |         Bb ... note intervals
- * Example usage:
- *     float bFlatBluesNoteFour = freqScaleBlues(kAs2, 3); // returns E3, 164.814 Hz
+/* getMovedKey
+ * utility function
+ * TODO write docs
  */
-
-float freqScaleBlues(int keyStart, int numAbove) {
-	// index: 0,  1, 2,  3, 4, 5, 6
-	// intvl: 1, 3b, 4, 5b, 5, 7, 8
-	// int scaleBluesIntervals[5] = {min3, per4, triT, per5, min7};
-	int scaleBluesIntervals[5] = {3, 5, 6, 7, 10};
-	// 12 - sBI[5] reversed = 2, 5, 6, 7, 9
-
+int getMovedKey(int keyStart, int numAbove, int* scaleIntervals, int lenScaleArray) {
+	
+	int numNotesOctave = lenScaleArray + 1;
+	
 	if (numAbove < 0) {							// if numAbove is negative, move the key down
-		while (numAbove <= -6) {					// bump an octave down every six blues notes
-			numAbove += 6;								// six keys in the blues scale
+		while (numAbove <= -numNotesOctave) {		// bump an octave down every six scale notes
+			numAbove += numNotesOctave;					// [octave] keys in the given scale
 			keyStart -= per8;							// equals one octave in the chromatic scale
 		}
 		if (numAbove < 0) {							// if numAbove is < 0, indicating there are non-octaves to take care of,
-			numAbove = 5 + numAbove;					// fix numAbove to refer to the proper interval index value
-			keyStart -= (12 - scaleBluesIntervals[numAbove]); // bump the key in the blues interval specified
+			numAbove = numNotesOctave - 1 + numAbove;	// fix numAbove to refer to the proper interval index value
+			keyStart -= (12 - scaleIntervals[numAbove]); // bump the key in the scale interval specified
 		}
 
 	} else if (numAbove > 0) {					// if numAbove is positive, move the key up
-		while (numAbove >= 6) { 					// bump an octave up every six blues notes
-			numAbove -= 6;								// six keys in the blues scale
+		while (numAbove >= numNotesOctave) { 		// bump an octave up every six scale notes
+			numAbove -= numNotesOctave;					// [octave] keys in the given scale
 			keyStart += per8;							// equals one octave in the chromatic scale
 		}
 		if (numAbove > 0) {							// if numAbove is > 0, indicating there are non-octaves to take care of,
-			keyStart += scaleBluesIntervals[numAbove - 1]; // bump the key in the blues interval specified
+			keyStart += scaleIntervals[numAbove - 1]; // bump the key in the scale interval specified
 		}
 	}
 
 	if (keyStart < 0 || keyStart > 87) {		// if the note reaches beyond the piano keys
 		return -1;									// return -1 error
 	} else { 									// otherwise
-		return pianoFreq[keyStart];					// return the freq requested
+		return keyStart;					// return the key requested
 	}
+}
+
+/* freqScaleMajor
+ * Takes in a starting keynum and a number of major scale steps to move up
+ * Returns the freq value of that note as a float
+ * The major scale in C is as follows:
+ *      C      D      E      F      G      A      B      C ..... notes
+ *      | maj2 | maj2 | min2 | maj2 | maj2 | maj2 | min2 | ..... interior intervals
+ *      | maj2 | maj3 | per4 | per5 | maj6 | maj7 | per8 | ..... [C ... note] intervals
+ * Example usage:
+ *     float cMajorNoteFive = freqScaleMajor(C4, 3); // returns (float)391.995 (Hz, G4)
+ */
+float freqScaleMajor(int keyStart, int numAbove) {
+	int scaleMajorIntervals[6] = {maj2, maj3, per4, per5, maj6, maj7};
+	int lenMajorArray = 6;
+	return pianoFreq[getMovedKey(keyStart, numAbove, scaleMajorIntervals, lenMajorArray)];
+}
+
+/* freqScaleMinor
+ * Takes in a starting keynum and a number of minor scale steps to move up
+ * Returns the freq value of that note as a float
+ * The minor scale in A is as follows:
+ *      A      B      C      D      E      F      G      A ..... notes
+ *      | maj2 | min2 | maj2 | maj2 | min2 | maj2 | maj2 | ..... interior intervals
+ *      | maj2 | min3 | per4 | per5 | min6 | min7 | per8 | ..... [C ... note] intervals
+ * Example usage:
+ *     float aMinorNoteThree = freqScaleMinor(A4, 3); // returns (float)523.251 (Hz, C5)
+ */
+float freqScaleMinor(int keyStart, int numAbove) {
+	int scaleMinorIntervals[6] = {maj2, min3, per4, per5, min6, min7};
+	int lenMinorArray = 6;
+	return pianoFreq[getMovedKey(keyStart, numAbove, scaleMinorIntervals, lenMinorArray)];
+}
+
+/* freqScaleBlues
+ * Takes in a starting keynum and a number of blues scale steps to move up
+ * Returns the freq value of that note as a float
+ * The blues scale in Bb is as follows:
+ *     Bb      Db      Eb      E      F      Ab      Bb .... notes
+ *      | min3  | maj2  | min2 | min2 | min3  | min2 | ..... interior intervals
+ *      | min3  | per4  | triT | per5 | min7  | per8 | ..... [Bb ... note] intervals
+ * Example usage:
+ *     float bFlatBluesNoteFour = freqScaleBlues(kAs2, 3); // returns (float)164.814 (Hz, E3)
+ */
+float freqScaleBlues(int keyStart, int numAbove) {
+	int scaleBluesIntervals[5] = {min3, per4, triT, per5, min7};
+	int lenBluesArray = 5;
+	return pianoFreq[getMovedKey(keyStart, numAbove, scaleBluesIntervals, lenBluesArray)];
 }
--- musicScalesTest.ino
+++ musicScalesTest.ino
@@ -1,38 +1,28 @@
 #include "musicScales.c"
 
 void setup() {
+
 	Serial.begin(115200);
-	float middleC = pianoFreq[kC4];
-	float fourFourty = pianoFreq[kA5];
-	float highC = pianoFreq[kC4 + 24];
-	Serial.println(middleC);
-	Serial.println(fourFourty);
-	Serial.println(highC);
-	float perfectFourth = pianoFreq[kC4 + per4];
-	float triTone = pianoFreq[kC4 + triT];
-	Serial.println(perfectFourth);
-	Serial.println(triTone);
-	Serial.println();
 
-	// this should print the freqs of a blues scale starting at Bb:
-	// Bb, D, Eb, E, F, Ab, Bb
-	for (int i = 0; i <= 6; i++) {
+	for (int i = 0; i <= 7; i++) {
 		Serial.print("Note ");
 		Serial.print(i);
 		Serial.print(": ");
-		Serial.println(freqScaleBlues(kAs2, i));
+		Serial.println(freqScaleMinor(kA4, i));
 	}
 	Serial.println();
-	for (int i = 0; i >= -6; i--) {
+
+	for (int i = 0; i >= -7; i--) {
 		Serial.print("Note ");
 		Serial.print(i);
 		Serial.print(": ");
-		Serial.println(freqScaleBlues(kAs2, i));
+		Serial.println(freqScaleMinor(kA4, i));
 	}
-
 	Serial.println();
 
-	Serial.println(freqScaleBlues(kAs2, 3));
+	Serial.println(freqScaleMinor(kA4, 3));
+	Serial.println();
+
 }
 
 void loop() {}
